e7ccf656ea66dd291bf21a935f1c2158adfd61ad
Jin Morikawa <forest611328@gmail.com>
2025-08-29 21:08:32 +0900

    refactor(requests): Deposit/Withdraw リクエストのデフォルト値を廃止し、必須項目として明示（required + [Required]）に変更

--- Man10BankService/Models/Requests/WithdrawRequest.cs
+++ Man10BankService/Models/Requests/WithdrawRequest.cs
@@ -1,13 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Man10BankService.Models.Requests;
 
 public class WithdrawRequest
 {
-    public string Uuid { get; set; } = string.Empty;
-    public string Player { get; set; } = string.Empty;
-    public decimal Amount { get; set; }
-    public string PluginName { get; set; } = "api";
-    public string Note { get; set; } = string.Empty;
-    public string DisplayNote { get; set; } = string.Empty;
-    public string Server { get; set; } = string.Empty;
-}
+    [Required]
+    public required string Uuid { get; set; }
 
+    [Required]
+    public required string Player { get; set; }
+
+    [Required]
+    public decimal Amount { get; set; }
+
+    [Required]
+    public required string PluginName { get; set; }
+
+    [Required]
+    public required string Note { get; set; }
+
+    [Required]
+    public required string DisplayNote { get; set; }
+
+    [Required]
+    public required string Server { get; set; }
+}
