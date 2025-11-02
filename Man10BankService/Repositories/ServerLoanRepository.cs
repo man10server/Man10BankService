bf49639ec6d2d4e7cddc1fce32522ca4e64996c8
Jin Morikawa <forest611328@gmail.com>
2025-11-02 20:58:18 +0900

    feat: 初回貸出の場合のみ最終支払日を更新する

--- Man10BankService/Repositories/ServerLoanRepository.cs
+++ Man10BankService/Repositories/ServerLoanRepository.cs
@@ -58,6 +58,10 @@ public class ServerLoanRepository(IDbContextFactory<BankDbContext> factory)
         switch (action)
         {
             case ServerLoanLogAction.Borrow:
+                if (loan.BorrowAmount == 0m)
+                {
+                    loan.LastPayDate = DateTime.UtcNow;
+                }
                 loan.BorrowAmount += delta;
                 break;
             case ServerLoanLogAction.Interest:
