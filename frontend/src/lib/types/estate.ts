cf297887337728928a84c4aa40a763c7c894cd0b
Jin Morikawa <forest611328@gmail.com>
2026-02-12 20:58:39 +0900

    fix

--- frontend/src/lib/types/estate.ts
+++ frontend/src/lib/types/estate.ts
@@ -1,18 +1,3 @@
-export type Estate = {
-  id: number
-  player: string
-  uuid: string
-  date: string
-  vault: number
-  bank: number
-  cash: number
-  estateAmount: number
-  loan: number
-  shop: number
-  crypto: number
-  total: number
-}
-
 export type EstateHistory = {
   id: number
   player: string
